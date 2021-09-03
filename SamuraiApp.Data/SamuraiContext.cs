bf0d95bf5d7ff1f9e0f5f641017647421deae577
Christoper <c.blake@hotmail.se>
2021-09-03 10:44:56 +0200

    Add Context (Wrapper for dealing with the Databases).

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    class SamuraiContext
+    public class SamuraiContext:DbContext
+        // Ef Core DbContext does the work against our database/persistent data.
     {
+        //Wrappers for Dealing with our Samurai and Qutoes Contexts
+        public DbSet<Samurai> Samurai { get; set; }
+        
+        public DbSet<Quote> Quotes { get; set; }
+
+        // In Ef core the Relationships are infered from the classes Samurai and Quotes.
     }
 }
