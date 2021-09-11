2b83358d11c0a32ffcd030e989ebf400b8335c50
Christoper <c.blake@hotmail.se>
2021-09-11 15:26:06 +0200

    Updated filtering on Quotes with Samurai ID. Very close to Pandas or Numpy thinking.

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -29,14 +29,24 @@ namespace SamuraiApp.UI
             //AddQuoteToExistingSamuraiWhileTracked();
             //AddQuoteToExistingSamuraiNotTracked();
             //ForeignKey_AddQuoteToExistingSamuraiNotTracked(); GOOD
-            //EagerLoadSamuraiWithQuotes(); //GOOD ALL OR FILTERED WITH INCLUDE
+            //EagerLoadSamuraiWithQuotes(); //GOOD - ALL OR FILTERED WITH INCLUDE One to Many Many to Many filter.
             //ProjectSomeProperties();
             //ProjectSamuraisWithQuotes(); // Filter, create anynomous class with aggregate(count) Owner + Like Vehicles
             //ExplicitLoadQuotes(); // Load an object then load related Objects (Tables or singles) Owner then Vehicles, Membership. WORK ON ONE OBJECT ONLY
             FilteringWithRelatedData(); // Does not include the table filtered on. 
+            ModifyingRelatedDataWhenTracked(); // And how Nav propreties reference and collections + nested looks like
+
             Console.Write("Press any key...");
             Console.ReadKey();
             
+        }
+
+        private static void ModifyingRelatedDataWhenTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes) //Extensions on Quotes) .ThenInclude(q=>q.Translations.//Extensions on Translations)
+                            .FirstOrDefault(s => s.Id == 2);//Extension on SAMURAI
+            samurai.Quotes[0].Text = "Did you hear that?";
+            _context.SaveChanges();
         }
 
         private static void FilteringWithRelatedData()
@@ -98,6 +108,9 @@ namespace SamuraiApp.UI
             var filterSamurai = _context.Samurais.FirstOrDefault(s => s.Id == 1);
             // Get filtered Samurai and Quotes by Quering quotes?
             var filterQuotesSamurai = _context.Quotes.Include(q => q.Samurai).ToList();
+            var filterQuoteSamurai = _context.Quotes.Where(q => q.SamuraiId == 1).ToList(); //THere we go..
+            // Think of it as a Table with Row of Foreign Keys. Set that Row to True or False == Id
+            // Then subset the table on that row.
         }
 
         private static void ForeignKey_AddQuoteToExistingSamuraiNotTracked(int samuraiId = 2)
