2faea9a49dac0c14e16f5f6fd8d21a5fbc831e14
Christoper <c.blake@hotmail.se>
2021-09-11 20:39:33 +0200

    Fixed the data.

---
+++ SamuraiApp.Data/Migrations/20210911140317_Apple.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class Apple : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -33,15 +33,29 @@ namespace SamuraiApp.UI
             //ProjectSomeProperties();
             //ProjectSamuraisWithQuotes(); // Filter, create anynomous class with aggregate(count) Owner + Like Vehicles
             //ExplicitLoadQuotes(); // Load an object then load related Objects (Tables or singles) Owner then Vehicles, Membership. WORK ON ONE OBJECT ONLY
-            FilteringWithRelatedData(); // Does not include the table filtered on. 
+            //FilteringWithRelatedData(); // Does not include the table filtered on. 
             //ModifyingRelatedDataWhenTracked(); // And how Nav propreties reference and collections + nested looks like
             //ModifyingRelatedDataWhenNOTTracked();
-            AddingNewSamuraiToAnExisingBattle();
-            ReturnOneBattleWithSamurais(); // ONE => FIRSTORDEFAULT
-            ReturnALLBattlesWithSamurais();
+            //AddingNewSamuraiToAnExisingBattle();
+            //ReturnOneBattleWithSamurais(); // ONE => FIRSTORDEFAULT
+            //ReturnALLBattlesWithSamurais(); // MANY .ToList()
+
+            //AddAllSamuraisToAllBattles();
             Console.Write("Press any key...");
             Console.ReadKey();
             
+        }
+
+        private static void AddAllSamuraisToAllBattles()
+            // Needs modified Logger.
+        {
+            var allBattles = _context.Battles.ToList();
+            var allSamurais = _context.Samurais.ToList().Where(s=>s.Id != 12).ToList();
+            foreach (var battle in allBattles)
+            {
+                battle.Samurais.AddRange(allSamurais);
+            }
+            _context.SaveChanges();
         }
 
         private static void ReturnALLBattlesWithSamurais()
